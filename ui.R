c79aaf267b1cf01713f2665ecc9ba48d80d3bb1d
Mayuka Ninomiya Kanda <mkanda2525@gmail.com>
2026-05-06 14:59:36 -0700

    Update GitHub Link

--- ui.R
+++ ui.R
@@ -575,7 +575,7 @@ ui <- page_navbar(
                 tags$ul(
                   tags$li(
                     a("GitHub Repository",
-                      href = "https://github.com/M-Ninomiya-Kanda/Shiny_AMMOA",
+                      href = "https://github.com/M-Ninomiya-Kanda/Shiny_AMMOA_web",
                       target = "_blank")
                   )
                 ),
